26b3f0f049f6b7ce239350686143202c3b998076
Bo Dai <bo.dai@aspireiq.com>
2024-07-30 21:03:37 -0400

    test code for anyhow & thiserror

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use ecosystem::MyError;
+use std::fs;
+
+fn main() -> Result<(), anyhow::Error>{
+    let filename = "non-existent-file";
+
+    let _fd = fs::File::open(filename).with_context(|| format!("Can not find file: {}", filename))?;
+
+    fail_with_error()?;
+    Ok(())
+}
+
+fn fail_with_error() -> Result<(), MyError> {
+    Err(MyError::Custom("This is a custom error".to_string()))
 }

72fa4b8d671bcd38ce08352564bcfbeff6bfaf2e
Bo Dai <bo.dai@aspireiq.com>
2024-07-30 22:08:39 -0400

    introduce tracing & tracing-subscriber

--- examples/err.rs
+++ examples/err.rs
@@ -1,3 +1,5 @@
+use anyhow::Context;
+use std::fs;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -21,4 +23,17 @@ pub struct BigError {
     b: Vec<String>,
     c: [u8; 64],
     d: u64,
+}
+
+fn main() -> Result<(), anyhow::Error>{
+    let filename = "non-existent-file";
+
+    let _fd = fs::File::open(filename).with_context(|| format!("Can not find file: {}", filename))?;
+
+    fail_with_error()?;
+    Ok(())
+}
+
+fn fail_with_error() -> Result<(), MyError> {
+    Err(MyError::Custom("This is a custom error".to_string()))
 }

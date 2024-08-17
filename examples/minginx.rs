ffb0bccc36298036be91958da13728b2ad78a6ae
Bo Dai <bo.dai@aspireiq.com>
2024-08-17 01:45:12 -0400

    update

--- examples/minginx.rs
+++ examples/minginx.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use tokio::{io, net::{TcpListener, TcpStream}};
-use tracing::{info, level_filters::LevelFilter};
+use tracing::{info, level_filters::LevelFilter, warn};
 use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -14,7 +14,7 @@ struct Config {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    let layer = Layer::new().pretty().with_filter(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
 
     let config = resolve_config();
@@ -42,7 +42,11 @@ async fn proxy(mut client: TcpStream, mut upstream: TcpStream) -> Result<()> {
     let (mut upstream_reader, mut upstream_writer) = upstream.split();
     let client_to_upstream = io::copy(&mut client_reader, &mut upstream_writer);
     let upstream_to_client = io::copy(&mut upstream_reader, &mut client_writer);
-    tokio::try_join!(client_to_upstream, upstream_to_client)?;
+
+    if let Err(e) = tokio::try_join!(client_to_upstream, upstream_to_client) {
+        warn!("Error: {}", e);
+    }
+
     Ok(())
 }
     
